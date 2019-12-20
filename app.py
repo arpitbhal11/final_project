6c4554919932711be8162254ac44e5b749ef0434
arpitbhal11 <arpitkumar.bhal@alumni.esade.edu>
2019-12-20 12:51:17 +0100

    update app.py

--- app.py
+++ app.py
@@ -4,9 +4,9 @@ import dash_html_components as html
 import plotly.graph_objs as go
 import pandas as pd
 
-# app = dash.Dash()
-app = dash.Dash(__name__)
-server=app.server
+app = dash.Dash()
+# app = dash.Dash(__name__)
+# server=app.server
 app.css.append_css({"external_url": "https://codepen.io/chriddyp/pen/bWLwgP.css"})
 
 
